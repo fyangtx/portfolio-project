355d8d2d3931824833f909335264adab70fdc3ee
frank yang <fyangtx@gmail.com>
2019-10-29 12:02:14 -0500

    Add blog details

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    image = models.ImageField(upload_to ='images/')
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField()
     body = models.TextField()
+    image = models.ImageField(upload_to ='images/')
+
+    # this function for db to use title as name of the record.
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        briefText = self.body
+        if(len(self.body) > 100):
+            briefText = self.body[:100] + '...'
+        return briefText
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
