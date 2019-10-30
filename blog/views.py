54899399385cee9c610001107c40b42fb976b1a0
frank yang <fyangtx@gmail.com>
2019-10-30 15:33:40 -0500

    try something

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from .models import Blog
 # Create your views here.
 def allblogs(request):
     blogs = Blog.objects
-    return render(request, 'blog/allblogs.html', {'blogs':blogs})
+
+    return render(request, 'blog/allblogs.html', {'blogs': blogs})
 
 def detail(request, blog_id):
     detailblog =get_object_or_404(Blog, pk=blog_id)
